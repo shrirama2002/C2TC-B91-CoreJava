737c4988a42223b222f4467fe980c0d6940192b0
Shrirama Kamath <shriramakamath2002@gmail.com>
2024-08-16 12:43:47 +0530

    add database connector class

---
+++ src/DatabaseConnection.java
@@ -0,0 +1,28 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/bus_reservation_system";
+    private static final String USER = "root";
+    private static final String PASSWORD = "password";
+
+    public static Connection connect() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void disconnect(Connection connection) {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
