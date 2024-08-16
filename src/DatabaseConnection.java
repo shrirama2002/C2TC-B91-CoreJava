1eacb6a72609beaa507f256012ed2f4ebfb1dc86
Shrirama Kamath <shriramakamath2002@gmail.com>
2024-08-16 12:57:30 +0530

    update this db connection  class

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -3,9 +3,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/bus_reservation_system";
+    private static final String URL = "jdbc:mysql://localhost:3306/bussystem";
     private static final String USER = "root";
-    private static final String PASSWORD = "password";
+    private static final String PASSWORD = "";
     public static Connection connect() {
         try {
             return DriverManager.getConnection(URL, USER, PASSWORD);
